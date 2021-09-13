5935d6b02192ac14dc6a329a6a77ebe82029c3f4
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2021-09-13 14:15:32 +0200

    relations models user post category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    // Relation to User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation to Category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
